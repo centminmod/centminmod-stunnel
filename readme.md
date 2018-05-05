eae0dbf8239c669a9732143f9aab61515c6d7617
George Liu <eva2000@centminmod.com>
2018-05-05 17:47:30 +1000

    initial commit

---
+++ readme.md
@@ -0,0 +1,99 @@
+stunnel install for CentOS 7 based [Centmin Mod LEMP stacks](https://centminmod.com/) only
+
+* custom ECC 256bit ECDSA ssl certificated based stunnel installation compiled against OpenSSL 1.1.1 using GCC 7.2.1 compiler with Gold linker
+
+```
+systemctl status stunnelx
+* stunnelx.service - TLS tunnel for network daemons
+   Loaded: loaded (/usr/lib/systemd/system/stunnelx.service; enabled; vendor preset: disabled)
+  Drop-In: /etc/systemd/system/stunnelx.service.d
+           `-limit.conf
+   Active: active (running) since Sat 2018-05-05 07:31:21 UTC; 9s ago
+ Main PID: 8653 (stunnel)
+   CGroup: /system.slice/stunnelx.service
+           `-8653 /usr/local/bin/stunnel /etc/stunnel/stunnel.conf
+
+May 05 07:31:21 host.domain.com systemd[1]: Starting TLS tunnel for network daemons...
+May 05 07:31:21 host.domain.com stunnel[8652]: LOG5[ui]: stunnel 5.45 on x86_64-pc-linux-gnu platform
+May 05 07:31:21 host.domain.com systemd[1]: Started TLS tunnel for network daemons.
+```
+
+```
+stunnel -version
+stunnel 5.45 on x86_64-pc-linux-gnu platform
+Compiled/running with OpenSSL 1.1.1-pre6 (beta) 1 May 2018
+Threading:PTHREAD Sockets:POLL,IPv6 TLS:ENGINE,FIPS,OCSP,PSK,SNI Auth:LIBWRAP
+ 
+Global options:
+RNDbytes               = 1024
+RNDfile                = /dev/urandom
+RNDoverwrite           = yes
+ 
+Service-level options:
+ciphers                = FIPS (with "fips = yes")
+ciphers                = HIGH:!DH:!aNULL:!SSLv2 (with "fips = no")
+curve                  = prime256v1
+debug                  = daemon.notice
+logId                  = sequential
+options                = NO_SSLv2
+options                = NO_SSLv3
+sessionCacheSize       = 1000
+sessionCacheTimeout    = 300 seconds
+stack                  = 65536 bytes
+TIMEOUTbusy            = 300 seconds
+TIMEOUTclose           = 60 seconds
+TIMEOUTconnect         = 10 seconds
+TIMEOUTidle            = 43200 seconds
+verify                 = none
+```
+
+```
+/opt/stunnel-dep/bin/openssl version -a
+OpenSSL 1.1.1-pre6 (beta) 1 May 2018
+built on: Sat May  5 07:28:12 2018 UTC
+platform: linux-x86_64
+options:  bn(64,64) rc4(16x,int) des(int) idea(int) blowfish(ptr) 
+compiler: ccache gcc -fPIC -pthread -m64  -Wa,--noexecstack -Wall -O3 -march=x86-64 -fuse-ld=gold -Wimplicit-fallthrough=0 -fcode-hoisting -DOPENSSL_USE_NODELETE -DL_ENDIAN -DOPENSSL_PIC -DOPENSSL_CPUID_OBJ -DOPENSSL_IA32_SSE2 -DOPENSSL_BN_ASM_MONT -DOPENSSL_BN_ASM_MONT5 -DOPENSSL_BN_ASM_GF2m -DSHA1_ASM -DSHA256_ASM -DSHA512_ASM -DKECCAK1600_ASM -DRC4_ASM -DMD5_ASM -DAES_ASM -DVPAES_ASM -DBSAES_ASM -DGHASH_ASM -DECP_NISTZ256_ASM -DX25519_ASM -DPADLOCK_ASM -DPOLY1305_ASM -DNDEBUG
+OPENSSLDIR: "/opt/stunnel-dep"
+ENGINESDIR: "/opt/stunnel-dep/lib/engines-1.1"
+Seeding source: os-specific
+```
+
+```
+openssl x509 -in /etc/pki/tls/certs/stunnel.pem -text -noout
+Certificate:
+    Data:
+        Version: 3 (0x2)
+        Serial Number:
+            9c:df:af:47:9d:59:bb:4c
+    Signature Algorithm: ecdsa-with-SHA256
+        Issuer: C=US, ST=California, L=Los Angeles, CN=host.domain.com
+        Validity
+            Not Before: May  5 07:42:25 2018 GMT
+            Not After : May  2 07:42:25 2028 GMT
+        Subject: C=US, ST=California, L=Los Angeles, CN=host.domain.com
+        Subject Public Key Info:
+            Public Key Algorithm: id-ecPublicKey
+                Public-Key: (256 bit)
+                pub: 
+                    04:2b:69:42:f7:2f:4b:2f:67:cf:68:5f:0a:01:02:
+                    db:0c:83:10:f4:a5:0e:ee:ef:e6:73:23:4e:d3:00:
+                    ab:9a:96:2d:08:a4:53:05:91:94:e6:57:89:9f:7c:
+                    8e:65:3b:47:82:93:2b:33:bc:b4:6a:0e:e0:83:78:
+                    52:ba:65:ff:be
+                ASN1 OID: prime256v1
+                NIST CURVE: P-256
+        X509v3 extensions:
+            X509v3 Subject Key Identifier: 
+                04:BD:64:85:FE:2B:98:3A:05:B6:CF:84:BC:69:E6:29:3E:A0:CD:BC
+            X509v3 Authority Key Identifier: 
+                keyid:04:BD:64:85:FE:2B:98:3A:05:B6:CF:84:BC:69:E6:29:3E:A0:CD:BC
+
+            X509v3 Basic Constraints: 
+                CA:TRUE
+    Signature Algorithm: ecdsa-with-SHA256
+         30:46:02:21:00:cf:c9:13:3b:a2:d9:ec:65:90:b4:a2:1e:5b:
+         67:8b:ca:5c:67:12:29:26:a2:0b:9e:6f:f3:0b:23:f0:25:e4:
+         91:02:21:00:88:ce:69:00:dc:41:7a:e4:8c:cb:2b:d6:f3:8d:
+         01:fb:f5:1b:ea:fe:94:28:ae:d2:a5:a0:51:fa:0b:61:58:0f
+```
---
+++ stunnel-install.sh
@@ -0,0 +1,313 @@
+#!/bin/bash
+#########################################################
+# stunnel installed as service name = stunnelx so as to
+# not conflict with system yum installed stunnel versions
+#
+# custom stunnel install for centmin mod lemp stacks with
+# performance in mind compiled against openssl 1.1.1 with
+# ecdsa instead of rsa based ssl certificates for better
+# scalability and performance out of the box
+# 
+# stunnel & openssl 1.1.1 are compiled against GCC 7.2.1 
+# compiler with Gold linker if detected on the server
+# 
+# written by George Liu (eva2000) https://centminmod.com
+#########################################################
+# variables
+#############
+VER='0.1'
+DT=$(date +"%d%m%y-%H%M%S")
+DIR_TMP='/svr-setup'
+
+STUNNEL_VERSION='5.45b8'
+# whether stunnel.conf redis is setup as client or server
+# default is to setup as redis server stunnel server end
+STUNNEL_CLIENT='n'
+# ecdsa or rsa
+STUNNEL_CERTTYPE='ecdsa'
+# stunnel ssl cert expiry date default in days
+STUNNEL_CERTEXPIRY='3650'
+# stunnel max open file descriptor limit
+STUNNEL_FD='1048576'
+# where openssl 1.1.1 libraries get installed
+STUNNEL_LIBDIR='/opt/stunnel-dep'
+# openssl 1.1.1 version
+STUNNEL_OPENSSLVER='1.1.1-pre6'
+# GCC 7.2.1 compile as march=native or march=x86-64
+# default x86-64
+MARCH_TARGETNATIVE='n'
+
+# ssl cert variables
+STUNNEL_HOSTNAME=$(hostname -f)
+SELFSIGNEDSSL_C='US'
+SELFSIGNEDSSL_ST='California'
+SELFSIGNEDSSL_L='Los Angeles'
+SELFSIGNEDSSL_O=''
+SELFSIGNEDSSL_OU=''
+#########################################################
+# functions
+#############
+if [ ! -d /etc/systemd/system ]; then
+  echo
+  echo "error: CentOS 7 not detected"
+  echo
+  exit
+fi
+
+if [ ! -d "$DIR_TMP" ]; then
+  echo
+  echo "error: Centmin Mod Install no detected"
+  echo
+  exit
+fi
+
+if [[ "$MARCH_TARGETNATIVE" = [yY] ]]; then
+  MARCH_TARGET='native'
+else
+  MARCH_TARGET='x86-64'
+fi
+
+install_openssl() {
+  if [[ -f /opt/rh/devtoolset-7/root/usr/bin/gcc && -f /opt/rh/devtoolset-7/root/usr/bin/g++ ]]; then
+    source /opt/rh/devtoolset-7/enable
+    EXTRA_CFLAGS=" -Wimplicit-fallthrough=0 -fcode-hoisting"
+    export CFLAGS="-march=${MARCH_TARGET} -fuse-ld=gold${EXTRA_CFLAGS}"
+    export CXXFLAGS="$CFLAGS"
+  fi
+  cd "$DIR_TMP"
+  if [ ! -f "openssl-${STUNNEL_OPENSSLVER}.tar.gz" ]; then
+    wget "https://www.openssl.org/source/openssl-${STUNNEL_OPENSSLVER}.tar.gz"
+  fi
+  rm -rf "openssl-${STUNNEL_OPENSSLVER}"
+  tar xzf "openssl-${STUNNEL_OPENSSLVER}.tar.gz"
+  cd "openssl-${STUNNEL_OPENSSLVER}"
+  make clean; make distclean
+  s_openssldir="$STUNNEL_LIBDIR"
+  CFLAGS="-march=${MARCH_TARGET} -fuse-ld=gold${EXTRA_CFLAGS}"
+  CXXFLAGS="$CFLAGS"
+  ./config $CFLAGS -Wl,--enable-new-dtags,-rpath=${s_openssldir}/lib --prefix=${s_openssldir} --openssldir=${s_openssldir} shared enable-ec_nistp_64_gcc_128 enable-tls1_3
+  make -j$(nproc)
+  make certs
+  make install
+}
+
+setup_configfile() {
+  if [[ "$STUNNEL_CLIENT" = [nN] ]]; then
+# server config
+cat > /etc/stunnel/stunnel.conf <<EOF
+client = no
+chroot = /var/run/stunnel
+cert = /etc/pki/tls/certs/stunnel.pem
+pid = /stunnel.pid
+#output = /var/log/stunnel.log
+output = /stunnel.log
+
+setuid = stunnel
+setgid = stunnel
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+
+[redis]
+accept = 7379
+connect = 127.0.0.1:6379
+cert = /etc/stunnel/server.crt
+key = /etc/stunnel/server.key
+CAfile = /etc/stunnel/client.crt
+verify = 3
+sessionCacheSize = 10000
+sessionCacheTimeout = 10
+EOF
+  elif [[ "$STUNNEL_CLIENT" = [yY] ]]; then
+# client config
+cat > /etc/stunnel/stunnel.conf <<EOF
+client = yes
+chroot = /var/run/stunnel
+cert = /etc/pki/tls/certs/stunnel.pem
+pid = /stunnel.pid
+#output = /var/log/stunnel.log
+output = /stunnel.log
+
+setuid = stunnel
+setgid = stunnel
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+
+[redis]
+accept = 127.0.0.1:8379
+connect = ${REDIS_REMOTEIP:-REDIS_REMOTEIP}:7379
+cert = /etc/stunnel/client.crt
+key = /etc/stunnel/client.key
+CAfile = /etc/stunnel/server.crt
+verify = 3
+sessionCacheSize = 10000
+sessionCacheTimeout = 10
+EOF
+  fi
+}
+
+setup_peercerts() {
+  pushd /etc/stunnel
+  if [[ "$STUNNEL_CERTTYPE" = 'ecdsa' ]]; then
+    # server
+    openssl ecparam -out server.key -name prime256v1 -genkey
+    openssl req -new -x509 -nodes -days 3650 -key server.key -out server.crt -subj "/C=${SELFSIGNEDSSL_C}/ST=${SELFSIGNEDSSL_ST}/L=${SELFSIGNEDSSL_L}/O=${SELFSIGNEDSSL_O}/OU=${SELFSIGNEDSSL_OU}/CN=${STUNNEL_HOSTNAME}"
+    openssl x509 -in server.crt -text -noout
+    chmod 0600 server.key
+    # client
+    openssl ecparam -out client.key -name prime256v1 -genkey
+    openssl req -new -x509 -nodes -days 3650 -key client.key -out client.crt -subj "/C=${SELFSIGNEDSSL_C}/ST=${SELFSIGNEDSSL_ST}/L=${SELFSIGNEDSSL_L}/O=${SELFSIGNEDSSL_O}/OU=${SELFSIGNEDSSL_OU}/CN=${STUNNEL_HOSTNAME}"
+    openssl x509 -in client.crt -text -noout
+    chmod 0600 client.key
+  else
+    # server
+    openssl genrsa -out server.key 2048
+    openssl req -new -x509 -nodes -days 3650 -key server.key -out server.crt -subj "/C=${SELFSIGNEDSSL_C}/ST=${SELFSIGNEDSSL_ST}/L=${SELFSIGNEDSSL_L}/O=${SELFSIGNEDSSL_O}/OU=${SELFSIGNEDSSL_OU}/CN=${STUNNEL_HOSTNAME}"
+    openssl x509 -in server.crt -text -noout
+    chmod 0600 server.key
+    # client
+    openssl genrsa -out client.key 2048
+    openssl req -new -x509 -nodes -days 3650 -key client.key -out client.crt -subj "/C=${SELFSIGNEDSSL_C}/ST=${SELFSIGNEDSSL_ST}/L=${SELFSIGNEDSSL_L}/O=${SELFSIGNEDSSL_O}/OU=${SELFSIGNEDSSL_OU}/CN=${STUNNEL_HOSTNAME}"
+    openssl x509 -in client.crt -text -noout
+    chmod 0600 client.key
+  fi
+  popd
+}
+
+setup_stunnel() {
+  if [[ "$STUNNEL_CERTTYPE" = 'ecdsa' ]]; then
+    # create stunnel.pem
+    echo "creating ecdsa based /etc/pki/tls/certs/stunnel.pem"
+    pushd /etc/pki/tls/certs/
+    umask 77
+    openssl ecparam -out stunnel.key -name prime256v1 -genkey
+    openssl req -new -x509 -nodes -days ${STUNNEL_CERTEXPIRY} -key stunnel.key -out stunnel.crt -subj "/C=${SELFSIGNEDSSL_C}/ST=${SELFSIGNEDSSL_ST}/L=${SELFSIGNEDSSL_L}/O=${SELFSIGNEDSSL_O}/OU=${SELFSIGNEDSSL_OU}/CN=${STUNNEL_HOSTNAME}"
+    openssl x509 -in stunnel.crt -text -noout
+    chmod 0600 stunnel.key
+    cat stunnel.key >  stunnel.pem
+    echo "" >> stunnel.pem
+    cat stunnel.crt >> stunnel.pem
+    rm -f stunnel.key stunnel.crt
+    popd
+    echo "created ecdsa based /etc/pki/tls/certs/stunnel.pem"
+  else
+    # create stunnel.pem
+    echo "creating rsa based /etc/pki/tls/certs/stunnel.pem"
+    pushd /etc/pki/tls/certs/
+    umask 77
+    PEM1=`/bin/mktemp /tmp/openssl.XXXXXX`
+    PEM2=`/bin/mktemp /tmp/openssl.XXXXXX`
+    openssl req -newkey rsa:2048 -keyout $PEM1 -nodes -x509 -days ${STUNNEL_CERTEXPIRY} -out $PEM2 -subj "/C=${SELFSIGNEDSSL_C}/ST=${SELFSIGNEDSSL_ST}/L=${SELFSIGNEDSSL_L}/O=${SELFSIGNEDSSL_O}/OU=${SELFSIGNEDSSL_OU}/CN=${STUNNEL_HOSTNAME}"
+    cat $PEM1 >  stunnel.pem
+    echo "" >> stunnel.pem
+    cat $PEM2 >> stunnel.pem
+    rm -f $PEM1 $PEM2
+    popd
+    echo "created rsa based /etc/pki/tls/certs/stunnel.pem"
+  fi
+  echo
+  echo "check /etc/pki/tls/certs/stunnel.pem"
+  echo "openssl x509 -in /etc/pki/tls/certs/stunnel.pem -text -noout"
+  openssl x509 -in /etc/pki/tls/certs/stunnel.pem -text -noout
+  echo
+  setup_peercerts
+  setup_configfile
+
+  systemctl daemon-reload
+  systemctl restart stunnelx.service
+  systemctl enable stunnelx.service
+  echo
+  systemctl status stunnelx.service
+  echo
+  stunnel -version
+  echo
+}
+
+install_stunnel() {
+  if [[ -f /opt/rh/devtoolset-7/root/usr/bin/gcc && -f /opt/rh/devtoolset-7/root/usr/bin/g++ ]]; then
+    unset CFLAGS
+    unset CXXFLAGS
+  fi
+  cd "$DIR_TMP"
+  if [ ! -f "stunnel-${STUNNEL_VERSION}.tar.gz" ]; then
+    wget "https://www.stunnel.org/downloads/beta/stunnel-${STUNNEL_VERSION}.tar.gz"
+  fi
+  rm -rf "stunnel-${STUNNEL_VERSION}.tar.gz"
+  tar xzf "stunnel-${STUNNEL_VERSION}.tar.gz"
+  cd stunnel-5.45
+  make clean; make distclean
+  LDFLAGS="-Wl,-rpath -Wl,${STUNNEL_LIBDIR}/lib" ./configure --with-ssl=${STUNNEL_LIBDIR}
+  make -j$(nproc)
+  make install
+  
+  mkdir -p /etc/stunnel
+  touch /var/log/stunnel.log
+  chmod 666 /var/log/stunnel.log
+  useradd -r -m -d /var/run/stunnel -s /bin/false stunnel
+  mkdir -p /var/run/stunnel/etc
+  \cp -af /etc/hosts.allow /etc/hosts.deny /var/run/stunnel/etc
+  chown -R stunnel:stunnel /var/run/stunnel
+  echo "d /var/run/stunnel 0770 stunnel stunnel -" > /etc/tmpfiles.d/stunnel.conf
+  
+  mkdir -p /etc/systemd/system/stunnelx.service.d
+  echo -e "[Service]\nLimitNOFILE=${STUNNEL_FD}\nLimitNPROC=${STUNNEL_FD}" > /etc/systemd/system/stunnelx.service.d/limit.conf
+
+cat > /usr/lib/systemd/system/stunnelx.service <<EOF
+[Unit]
+Description=TLS tunnel for network daemons
+After=syslog.target network.target
+
+[Service]
+ExecStart=/usr/local/bin/stunnel /etc/stunnel/stunnel.conf
+Type=forking
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+stunnel_check() {
+  echo
+  "$STUNNEL_LIBDIR/bin/openssl" version -a
+  echo
+  /usr/local/bin/stunnel -version
+  echo
+}
+
+#########################################################
+case $1 in
+  install )
+    install_openssl
+    install_stunnel
+    setup_stunnel
+    ;;
+  update )
+    install_openssl
+    install_stunnel
+    systemctl daemon-reload
+    systemctl restart stunnelx.service
+    echo
+    systemctl status stunnelx.service
+    ;;
+  reinstall )
+    install_openssl
+    install_stunnel
+    systemctl daemon-reload
+    systemctl restart stunnelx.service
+    echo
+    systemctl status stunnelx.service
+    ;;
+  check )
+    stunnel_check
+    ;;
+  openssl )
+    install_openssl
+    ;;
+  * )
+    echo
+    echo "Usage:" 
+    echo "$0 {install|update|reinstall|check|openssl"
+    echo
+    ;;
+esac
+exit
